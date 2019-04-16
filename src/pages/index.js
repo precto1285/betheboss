286357dadb13f01b75e1ffa0978eeb2164e4c829
Philippe Recto <precto1285@gmal.com>
2019-04-16 16:49:09 -0700

    added form and css row and column for other content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import 'bootstrap/dist/css/bootstrap.css'
-import { Container, Row, Jumbotron, Button, Card } from 'reactstrap'
+import { Container, Row, Jumbotron, Button, Card, Form, FormGroup, FormText, Label, Input } from 'reactstrap'
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
@@ -47,40 +47,51 @@ const IndexPage = () => (
       <Jumbotron>
         <h2> Receive free business resources and learn more about our incredible intern program by signing up.</h2>
       </Jumbotron>
-
-      <p>Intern Expectations</p>
-      <ul>
-        <li>
-          Interns should expect a total of at least 6 hours a week to successfully
-          complete the program over the course of 8 weeks
+      <Row>
+        <div className="col-md-6">
+          <h4>Intern Expectations</h4>
+          <ul>
+            <li>
+              Interns should expect a total of at least 6 hours a week to successfully
+              complete the program over the course of 8 weeks
         </li>
-        <li>
-          Interns will spend 2 hours a week actively participating in each session
-          over the course of the 8 week (2 of the sessions will be a fun team
-          building event and graduation)
+            <li>
+              Interns will spend 2 hours a week actively participating in each session
+              over the course of the 8 week (2 of the sessions will be a fun team
+              building event and graduation)
         </li>
-        <li>
-          Interns will touch base with their mentor remotely for at least an hour
-          a week to receive assistance and support on each week’s lessons
+            <li>
+              Interns will touch base with their mentor remotely for at least an hour
+              a week to receive assistance and support on each week’s lessons
         </li>
-        <li>
-          Interns will be provided with a refurbished laptop in working condition
-          for the duration of the program and will be expected to return the
-          laptop at the end of the program or if the intern cannot for any reason
-          complete the program
+            <li>
+              Interns will be provided with a refurbished laptop in working condition
+              for the duration of the program and will be expected to return the
+              laptop at the end of the program or if the intern cannot for any reason
+              complete the program
         </li>
-      </ul>
-      <p>Application</p>
-      <p>Name</p>
-      <p>Age </p>
-      <p>Why do you want to take part in this internship program?</p>
-      <p>Do you have any hobbies?</p>
-      <p>What do you like to do in your free time?</p>
-      <p>What are you passionate about? How do you express your passion in your life?</p>
-      <p>Name someone who you look at as a role model and explain why you see them in that way?</p>
-      <p>Are you prepared to devote at least 6 hours a week over the summer for 8 weeks in order
-        to successfully complete the program?
+          </ul>
+        </div>
+        <div className="col-md-6">
+          <Form>
+            <h4>Application:</h4>
+            <FormGroup>
+              <Label>Name:</Label>
+              <Input type="text"></Input>
+              <Label>Age:</Label>
+              <Input type="text"></Input>
+            </FormGroup>
+            <p>Why do you want to take part in this internship program?</p>
+            <p>Do you have any hobbies?</p>
+            <p>What do you like to do in your free time?</p>
+            <p>What are you passionate about? How do you express your passion in your life?</p>
+            <p>Name someone who you look at as a role model and explain why you see them in that way?</p>
+            <p>Are you prepared to devote at least 6 hours a week over the summer for 8 weeks in order
+              to successfully complete the program?
       </p>
+          </Form>
+        </div>
+      </Row>
     </Container>
 
 
