564693bc17a235a30a1322641447439407e2c015
Philippe Recto <precto1285@gmal.com>
2019-04-16 15:33:46 -0700

    updated css with bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,41 +10,40 @@ import Kids from '../images/be-the-boss-business.jpg'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Be The Boss Program</h1>
     <Container>
-      <Row className="my-3">
+      <div className="my-5 text-dark text-center">
+        <h1>Be The Boss Program</h1>
+      </div>
+      <Row className="my-3 bg-light p-3">
         <p>Our Be The Boss Internship Program transforms youth and young adults (ages 16-24) into confident and successful  entrepreneurs. </p>
-      </Row>
-      <Row>
+
         <p>Through our six-week program, interns generate business ideas, conduct market research, write business plans, pitch to a panel of investors, and launch their very own businesses.</p>
       </Row>
+
       <Row className="my-5">
         <div className="col-lg-6">
           <Card>
             <div className="card-header">
-              <h3>Intern criteria requirements:</h3>
+              <h4>Intern criteria requirements:</h4>
             </div>
-            <ul className="mt-4 px-3">
-              <li>16-24 years old</li>
-              <li>Qualify as low to moderate income</li>
-              <li>Part of a demographic group underrepresented in entrepreneurship including women and minorities</li>
-              <li>Does not have access to a computer at home</li>
-              <li>Does not have access to entrepreneurial resources from a family network</li>
-            </ul>
+            <div className="card-body">
+              <ul className="mt-2 px-3">
+                <li>16-24 years old</li>
+                <li>Qualify as low to moderate income</li>
+                <li>Part of a demographic group underrepresented in entrepreneurship including women and minorities</li>
+                <li>Does not have access to a computer at home</li>
+                <li>Does not have access to entrepreneurial resources from a family network</li>
+              </ul>
+            </div>
+            <div className="card-footer">
+              <Button className="btn-block btn-danger">Support Our Work</Button>
+            </div>
           </Card>
         </div>
-        <div className="col-lg-6 mt-">
-          <Card>
-            <img src={Kids} />
-            <div className="card-footer">
-              <br />
-              <center><Button className="btn btn-danger">Support Our Work</Button></center>
-              <br />
-            </div>
-          </Card>
+        <div className="col-lg-6 mt-5">
+          <img src={Kids} />
         </div>
       </Row>
-
       <Jumbotron>
         <h2> Receive free business resources and learn more about our incredible intern program by signing up.</h2>
       </Jumbotron>
