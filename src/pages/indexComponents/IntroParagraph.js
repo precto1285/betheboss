193abb72bd6b7c425167c5bff6db4bdd6ba83d4e
Philippe Recto <precto1285@gmal.com>
2019-04-17 13:28:42 -0700

    edited redundant paragraph

--- src/pages/indexComponents/IntroParagraph.js
+++ src/pages/indexComponents/IntroParagraph.js
@@ -6,7 +6,7 @@ const IntroParagraph = () => {
   return (
     <Card>
       <div className="card-body p-3">
-        <p>Our Be The Boss Internship Passionpreneurship Program transforms youth and young adults (ages 16-24) into confident and successful  entrepreneurs. </p>
+        <p>Our Be The Boss Passionpreneurship Program transforms youth and young adults (ages 16-24) into confident and successful  entrepreneurs. </p>
 
         <p>Through our six-week program, interns generate business ideas, conduct market research, write business plans, pitch to a panel of investors, and launch their very own businesses.</p>
       </div>
