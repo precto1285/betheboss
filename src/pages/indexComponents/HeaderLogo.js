b2dd5d23165f76a334d2e7ceabaad3ce8fb8db90
Philippe Recto <precto1285@gmal.com>
2019-04-18 12:53:21 -0700

    slight change on HeaderLogo

--- src/pages/indexComponents/HeaderLogo.js
+++ src/pages/indexComponents/HeaderLogo.js
@@ -6,7 +6,7 @@ import './HeaderImg.css';
 const HeaderLogo = () => {
   return (
     <div className="JumbotronImg py-5">
-      <Row className="mb-3">
+      <Row className="mb-4">
         <Col className="col-sm-6 py-3">
           <img className="idea" src={Logo} alt='icon' width="70%" height="100%" />
         </Col>
